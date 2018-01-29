eac712881b694d59e98f3aba3d6f1b058a8cbf92
johnhigginsavila <johnhiggins.avila@gmail.com>
2018-01-30 03:40:43 +0800

    [#HOST]FEAT(REDUX): added redux store

--- src/app/config/api-url.ts
+++ src/app/config/api-url.ts
@@ -1,3 +1,3 @@
 import { isDevMode } from '@angular/core';
 
-export const BACKEND_URL: string = (isDevMode()) ? 'http://127.0.0.1:5000' : ''; // place backend url here
+export const BACKEND_URL: string = (isDevMode()) ? 'http://127.0.0.1:5000' : 'https://straatinfo-backend-v2.herokuapp.com'; // place backend url here
