d119934aaf3e1d826344502b62061d049c9a769d
markatil <markmey.fedalizoatil@gmail.com>
2018-02-20 17:49:01 +0800

    [#HOST] Host Reporter Download data

--- src/app/host/host-reporter-list/host-reporter-list.component.ts
+++ src/app/host/host-reporter-list/host-reporter-list.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import { select } from '@angular-redux/store';
 import { ReporterActionCreator } from '../../store/action-creators';
+import { Angular2Csv } from 'angular2-csv/Angular2-csv';
 
 @Component({
   selector: 'app-host-report-list',
@@ -13,6 +14,7 @@ export class HostReporterListComponent implements OnInit {
 
   private routeParamsSubscription: Subscription = null;
   private hostSubscription: Subscription = null;
+  private reportSubscription: Subscription = null;
 
   @select(s => s.reporter.reporters) reporters;
   @select(s => s.reporter.spinner) reporterSpinner;
@@ -21,6 +23,7 @@ export class HostReporterListComponent implements OnInit {
 
   public hostName: string;
   public hostId: string;
+  public reporterData = [];
 
   public dataNames: string[] = [
       'fname', 'lname', '_chat', 'isVolunteer', '_team'
@@ -50,6 +53,11 @@ export class HostReporterListComponent implements OnInit {
         .subscribe(host => {
             this.hostName = host.hostName;
         });
+
+    this.reportSubscription = this.reporters
+        .subscribe(reporter => {
+            this.reporterData = reporter;
+        });
   }
 
   onBack() {
@@ -59,5 +67,62 @@ export class HostReporterListComponent implements OnInit {
   onMoreClick(event) {
       this.reporterActionCreator.SelectReporter(event._id);
       this.router.navigate([`admin/reporter/${event._id}`]);
+  }
+
+  onDownload() {
+      const date = new Date();
+      const year = date.getFullYear().toString();
+      const month = this.padLeft((date.getMonth() + 1).toString(), '0', 2);
+      const day = this.padLeft(date.getDate().toString(), '0', 2);
+      const hour = this.padLeft(date.getHours().toString(), '0', 2);
+      const minutes = this.padLeft(date.getMinutes().toString(), '0', 2);
+      const formattedDate = year + month + day + "_" + hour + minutes;
+
+      var options = {
+          fieldSeparator: ',',
+          quoteStrings: '"',
+          decimalseparator: '.',
+          showLabels: true,
+          showTitle: false,
+          useBom: true
+      };
+
+      var mapData = [];
+      mapData.push(this.ReporterHeader());
+      this.reporterData.map(d => mapData.push(this.ReporterData(d)));
+      var fileName = 'Reporters_' + this.hostName + '_' + formattedDate;
+      new Angular2Csv(mapData, fileName, options);
+  }
+
+  private ReporterData(data: any): any {
+      return {
+          id: data._id,
+          fname: data.fname,
+          lname: data.lname,
+          chatName: data._chat,
+          isVolunteer: data.isVolunteer,
+          team: data._team,
+          host: data._host,
+          status1: data.status1,
+          status2: data.status2,
+      };
+  }
+
+  private ReporterHeader(): any {
+      return {
+          id: "Id",
+          fname: "First Name",
+          lname: "Last Name",
+          chatName: "Chat Name",
+          isVolunteer: "Volunteer",
+          team: "Team",
+          host: "Host",
+          status1: "Status 1",
+          status2: "Status 2",
+      };
+  }
+
+  private padLeft(text: string, padChar: string, size: number): string {
+      return (String(padChar).repeat(size) + text).substr((size * -1), size);
   }
 }
