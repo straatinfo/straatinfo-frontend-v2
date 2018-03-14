c55e18c84882346a7e3c47642b82035ff68891b2
markatil <markmey.fedalizoatil@gmail.com>
2018-03-14 14:31:34 +0800

    [#REFACTOR] Updates

--- src/app/host/host-design-detail/host-design-detail.component.ts
+++ src/app/host/host-design-detail/host-design-detail.component.ts
@@ -8,6 +8,7 @@ import swal from 'sweetalert2';
 
 import { DesignActionCreator } from '../../store/action-creators';
 import { IDesign } from 'app/interface/design/design.interface';
+import { IDesignStore } from '../../store/design.store';
 
 import { BACKEND_URL } from '../../config';
 import { IDesignView } from '../../interface/design/design-view.interface';
@@ -15,140 +16,140 @@ import { IAppState } from '../../store/app.store';
 import { DESIGN_UPDATE_FULFILLED, DESIGN_UPDATE_FAILED, DESIGN_SELECT_FULFILLED } from '../../store/actions/design.action';
 
 @Component({
-	selector: 'app-host-design-detail',
-	templateUrl: './host-design-detail.component.html',
-	styleUrls: ['./host-design-detail.component.scss']
+    selector: 'app-host-design-detail',
+    templateUrl: './host-design-detail.component.html',
+    styleUrls: ['./host-design-detail.component.scss']
 })
 
-export class HostDesignDetailComponent implements OnInit ,DoCheck ,OnDestroy {
+export class HostDesignDetailComponent implements OnInit, DoCheck, OnDestroy {
 
-	public uploadUrl: string;
-	public hostId: string;
-	public hostDesignForm: FormGroup;
-	private routeParamsSubscription: Subscription = null;
-	private designSubscription: Subscription = null;
-	private designErrorSubscription: Subscription = null;
-	public errorText: string = null;
-	public successText: string = null;
-	// variables to check
-	public designData: IDesignView = null
-	public loadDesignData: boolean = false;
-	public errorMessage: string = null;
-	public successMessage: string = null;
+    public uploadUrl: string;
+    public hostId: string;
+    public hostDesignForm: FormGroup;
+    private routeParamsSubscription: Subscription = null;
+    private designSubscription: Subscription = null;
+    private designErrorSubscription: Subscription = null;
+    public errorText: string = null;
+    public successText: string = null;
+    // variables to check
+    public designData: IDesignView = null
+    public loadDesignData: boolean = false;
+    public errorMessage: string = null;
+    public successMessage: string = null;
 
-	@select(s => s.design.error) designStoreError: Observable<string>;
-	@select(s => s.design.selectedDesign) selectedDesign: Observable<IDesignView>;
-	@select(s => s.design.spinner) designSpinner: Observable<boolean>;
-	@select(s => s.misc.spinner) miscSpinner: Observable<boolean>;
-	constructor(
-		private actvatedRoute: ActivatedRoute,
-		private formBuilder: FormBuilder,
-		private router: Router,
-		private designActionCreator: DesignActionCreator,
-		private ngRedux: NgRedux<IAppState>
-	) {
-	
-	}
+    @select(s => s.design.error) designStoreError: Observable<string>;
+    @select(s => s.design.selectedDesign) selectedDesign: Observable<IDesignView>;
+    @select(s => s.design.spinner) designSpinner: Observable<boolean>;
+    @select(s => s.misc.spinner) miscSpinner: Observable<boolean>;
+    @select(s => s.design) design$: Observable<IDesignStore>;
 
-	ngOnInit() {
+    constructor(
+        private actvatedRoute: ActivatedRoute,
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private designActionCreator: DesignActionCreator,
+        private ngRedux: NgRedux<IAppState>
+    ) {
 
-		this.routeParamsSubscription = this.actvatedRoute.params
-			.subscribe(
-				params => {
-					this.uploadUrl = `${BACKEND_URL}/v1/api/design/${params._id}?flat=true`;
-					this.designActionCreator.SelectDesign(params._id);
-					this.designSubscription = this.selectedDesign
-						.subscribe(
-							(design: IDesignView) => {
-								this.hostId = (design) ? design._host: null;
-								this.designData = design;
-								this.loadDesignData = true;
-							}
-						);
-				}
-			);
-	}
+    }
 
-	ngDoCheck() {
-		if (this.designData && this.loadDesignData) this.onLoadDesignForm(this.designData);
-		if (this.successMessage) this.onSuccessMessage(this.successMessage);
-		if (this.errorMessage) this.onErrorMessage(this.errorMessage);
-	}
+    ngOnInit() {
 
-	ngOnDestroy() {
-		(this.routeParamsSubscription) ? this.routeParamsSubscription.unsubscribe() : null;
-		(this.designSubscription) ? this.designSubscription.unsubscribe() : null;
-		(this.designErrorSubscription) ? this.designErrorSubscription.unsubscribe() : null;
-	}
+        this.routeParamsSubscription = this.actvatedRoute.params
+            .subscribe(
+            params => {
+                this.uploadUrl = `${BACKEND_URL}/v1/api/design/${params._id}?flat=true`;
+                this.designActionCreator.SelectDesign(params._id);
+                this.designSubscription = this.selectedDesign
+                    .subscribe(
+                    (design: IDesignView) => {
+                        this.hostId = (design) ? design._host : null;
+                        this.designData = design;
+                        this.loadDesignData = true;
+                    }
+                    );
+            }
+            );
+    }
 
-	onLoadDesignForm(design: IDesignView) {
-		this.hostDesignForm = this.formBuilder.group({
-			_id: [design._id, Validators.required],
-			designName: [design.designName, Validators.required],
-			colorOne: [design.colorOne, Validators.required],
-			colorTwo: [design.colorTwo, Validators.required],
-			colorThree: [design.colorThree, Validators.required],
-			_profilePicSecureUrl: [design._profilePicSecureUrl, Validators.required],
-		});
-	}
+    ngDoCheck() {
+        if (this.designData && this.loadDesignData) this.onLoadDesignForm(this.designData);
+        if (this.successMessage) this.onSuccessMessage(this.successMessage);
+        if (this.errorMessage) this.onErrorMessage(this.errorMessage);
+    }
 
-	onColorOneEvent(value: string) {
-		this.loadDesignData = false;
-		// this.hostDesignForm.value.patch({ colorOne: value });
-		this.hostDesignForm.value.colorOne = value;
-	}
+    ngOnDestroy() {
+        (this.routeParamsSubscription) ? this.routeParamsSubscription.unsubscribe() : null;
+        (this.designSubscription) ? this.designSubscription.unsubscribe() : null;
+        (this.designErrorSubscription) ? this.designErrorSubscription.unsubscribe() : null;
+    }
 
-	onColorTwoEvent(value: string) {
-		this.loadDesignData = false;
-		// this.hostDesignForm.value.patch({ colorTwo: value });
-		this.hostDesignForm.value.colorTwo = value;
-	}
+    onLoadDesignForm(design: IDesignView) {
+        this.hostDesignForm = this.formBuilder.group({
+            _id: [design._id, Validators.required],
+            designName: [design.designName, Validators.required],
+            colorOne: [design.colorOne, Validators.required],
+            colorTwo: [design.colorTwo, Validators.required],
+            colorThree: [design.colorThree, Validators.required],
+            _profilePicSecureUrl: [design._profilePicSecureUrl, Validators.required],
+        });
+    }
 
-	onColorThreeEvent(value: string) {
-		this.loadDesignData = false;
-		// this.hostDesignForm.value.patch({ colorThree: value });
-		this.hostDesignForm.value.colorThree = value;
-	}
+    onColorOneEvent(value: string) {
+        this.loadDesignData = false;
+        // this.hostDesignForm.value.patch({ colorOne: value });
+        this.hostDesignForm.value.colorOne = value;
+    }
 
-	onBack() {
-		this.router.navigate([`admin/host/design/${this.hostId}`]);
-	}
+    onColorTwoEvent(value: string) {
+        this.loadDesignData = false;
+        // this.hostDesignForm.value.patch({ colorTwo: value });
+        this.hostDesignForm.value.colorTwo = value;
+    }
 
-	onErrorMessage(error: string) {
-		this.errorText = error;
-		this.successText = null;
-	}
+    onColorThreeEvent(value: string) {
+        this.loadDesignData = false;
+        // this.hostDesignForm.value.patch({ colorThree: value });
+        this.hostDesignForm.value.colorThree = value;
+    }
 
-	onSuccessMessage(success: string) {
-		this.successText = success;
-		this.errorText = null;
-	}
+    onBack() {
+        this.router.navigate([`admin/host/design/${this.hostId}`]);
+    }
 
-	onUploadError(event) {
-		this.ngRedux.dispatch({ type: DESIGN_UPDATE_FAILED, payload: event });
-		this.ngOnInit();
-	}
+    onErrorMessage(error: string) {
+        this.errorText = error;
+        this.successText = null;
+    }
 
-	onUploadSuccess(event) {
-		this.ngRedux.dispatch({ type: DESIGN_UPDATE_FULFILLED, payload: event });
-		this.ngRedux.dispatch({ type: DESIGN_SELECT_FULFILLED, payload: event });
-		this.loadDesignData = true;
-		this.ngOnInit();
-	}
+    onSuccessMessage(success: string) {
+        this.successText = success;
+        this.errorText = null;
+    }
 
-	onUpdate() {
-		this.errorText = null;
-		this.successText = null;
-		this.designActionCreator.UpdateDesign(this.hostDesignForm.value._id, this.hostDesignForm.value);
-		this.designErrorSubscription = this.designStoreError.subscribe(
-			error => {
-				if (error) {
-					console.log(error);
-					this.errorText = error;
-				} else {
-					this.successText = 'Design has been updated.';
-				}
-			}
-		);
-	}
+    onUploadError(event) {
+        this.ngRedux.dispatch({ type: DESIGN_UPDATE_FAILED, payload: event });
+        this.ngOnInit();
+    }
+
+    onUploadSuccess(event) {
+        this.ngRedux.dispatch({ type: DESIGN_UPDATE_FULFILLED, payload: event });
+        this.ngRedux.dispatch({ type: DESIGN_SELECT_FULFILLED, payload: event });
+        this.loadDesignData = true;
+        this.ngOnInit();
+    }
+
+    onUpdate() {
+        this.loadDesignData = true;
+        this.errorText = null;
+        this.successText = null;
+        this.designActionCreator.UpdateDesign(this.hostDesignForm.value._id, this.hostDesignForm.value);
+        this.designErrorSubscription = this.design$
+            .subscribe(
+            (design: IDesignStore) => {
+                if (design.error) this.errorMessage = design.error;
+                if (design.success) this.successMessage = design.success;
+            }
+            );
+    }
 }
--- src/app/store/action-creators/design.actioncreator.ts
+++ src/app/store/action-creators/design.actioncreator.ts
@@ -9,21 +9,21 @@ import { DesignService } from '../../services';
 import { IDesign } from '../../interface/design/design.interface';
 
 import {
-	DESIGN_CREATE_ATTEMPT,
-	DESIGN_CREATE_FAILED,
-	DESIGN_CREATE_FULFILLED,
-	DESIGN_DELETE_ATTEMPT,
-	DESIGN_DELETE_FAILED,
-	DESIGN_DELETE_FULFILLED,
-	DESIGN_GET_ATTEMPT,
-	DESIGN_GET_FAILED,
-	DESIGN_GET_FULFILLED,
-	DESIGN_UPDATE_ATTEMPT,
-	DESIGN_UPDATE_FAILED,
-	DESIGN_UPDATE_FULFILLED,
-	DESIGN_SELECT_FULFILLED,
-	DESIGN_SELECT_FAILED,
-	DESIGN_SELECT_ATTEMPT
+DESIGN_CREATE_ATTEMPT,
+DESIGN_CREATE_FAILED,
+DESIGN_CREATE_FULFILLED,
+DESIGN_DELETE_ATTEMPT,
+DESIGN_DELETE_FAILED,
+DESIGN_DELETE_FULFILLED,
+DESIGN_GET_ATTEMPT,
+DESIGN_GET_FAILED,
+DESIGN_GET_FULFILLED,
+DESIGN_UPDATE_ATTEMPT,
+DESIGN_UPDATE_FAILED,
+DESIGN_UPDATE_FULFILLED,
+DESIGN_SELECT_FULFILLED,
+DESIGN_SELECT_FAILED,
+DESIGN_SELECT_ATTEMPT
 } from '../actions/design.action';
 import { IDesignView } from '../../interface/design/design-view.interface';
 
@@ -32,154 +32,155 @@ import { IDesignView } from '../../interface/design/design-view.interface';
 
 export class DesignActionCreator implements OnDestroy {
 
-	private errorMessage: string;
-	private getDesignBySubscription: Subscription = null;
-	private getDesignByIdSubscription: Subscription = null;
-	private updateDesignSubscription: Subscription = null;
-	private createDesignSubscription: Subscription = null;
-	private selectDesignSubscription: Subscription = null;
+    private errorMessage: string;
+    private getDesignBySubscription: Subscription = null;
+    private getDesignByIdSubscription: Subscription = null;
+    private updateDesignSubscription: Subscription = null;
+    private createDesignSubscription: Subscription = null;
+    private selectDesignSubscription: Subscription = null;
 
-	constructor(
-		private ngRedux: NgRedux<IAppState>,
-		private router: Router,
-		private designService: DesignService
-	) { }
+    constructor(
+        private ngRedux: NgRedux<IAppState>,
+        private router: Router,
+        private designService: DesignService
+    ) { }
 
-	ngOnDestroy() {
-		(this.getDesignBySubscription) ? this.getDesignBySubscription.unsubscribe() : null;
-		(this.getDesignByIdSubscription) ? this.getDesignByIdSubscription.unsubscribe() : null;
-		(this.updateDesignSubscription) ? this.updateDesignSubscription.unsubscribe() : null;
-		(this.createDesignSubscription) ? this.createDesignSubscription.unsubscribe() : null;
-		(this.selectDesignSubscription) ? this.selectDesignSubscription.unsubscribe() : null;
-	}
+    ngOnDestroy() {
+        (this.getDesignBySubscription) ? this.getDesignBySubscription.unsubscribe() : null;
+        (this.getDesignByIdSubscription) ? this.getDesignByIdSubscription.unsubscribe() : null;
+        (this.updateDesignSubscription) ? this.updateDesignSubscription.unsubscribe() : null;
+        (this.createDesignSubscription) ? this.createDesignSubscription.unsubscribe() : null;
+        (this.selectDesignSubscription) ? this.selectDesignSubscription.unsubscribe() : null;
+    }
 
-	GetDesignByHostId(_hostId: string, flat: boolean = true) {
-		this.ngRedux.dispatch({ type: DESIGN_GET_ATTEMPT });
-		this.getDesignBySubscription = this.designService.GetDesignByHostId(_hostId, flat)
-			.map((data: IDesign[]) => { return data.map(d => this.ToDesignView(d)); })
-			.subscribe(
-				(design: IDesignView[]) => {
-					this.ngRedux.dispatch({ type: DESIGN_GET_FULFILLED, payload: design });
-				}, err => {
-					this.errorMessage = err._body;
-					if (this.errorMessage && typeof this.errorMessage === 'string') {
-						this.ngRedux.dispatch({ type: DESIGN_GET_FAILED, error: this.errorMessage });
-					}
-				},
-				() => {
-					this.errorMessage = null;
-				}
-			);
-	}
+    GetDesignByHostId(_hostId: string, flat: boolean = true) {
+        this.ngRedux.dispatch({ type: DESIGN_GET_ATTEMPT });
+        this.getDesignBySubscription = this.designService.GetDesignByHostId(_hostId, flat)
+            .map((data: IDesign[]) => { return data.map(d => this.ToDesignView(d)); })
+            .subscribe(
+            (design: IDesignView[]) => {
+                this.ngRedux.dispatch({ type: DESIGN_GET_FULFILLED, payload: design });
+            }, err => {
+                this.errorMessage = err._body;
+                if (this.errorMessage && typeof this.errorMessage === 'string') {
+                    this.ngRedux.dispatch({ type: DESIGN_GET_FAILED, error: this.errorMessage });
+                }
+            },
+            () => {
+                this.errorMessage = null;
+            }
+            );
+    }
 
-	GetDesignById(_id: string, flat: boolean = true) {
-		this.ngRedux.dispatch({ type: DESIGN_SELECT_ATTEMPT });
-		this.getDesignByIdSubscription = this.designService.GetDesignById(_id, flat)
-			.map(data => this.ToDesignView(data))
-			.subscribe(
-				(design: IDesignView) => {
-					this.ngRedux.dispatch({ type: DESIGN_SELECT_FULFILLED, payload: design });
-				}, err => {
-					this.errorMessage = err._body;
-					if (this.errorMessage && typeof this.errorMessage === 'string') {
-						this.ngRedux.dispatch({ type: DESIGN_SELECT_FAILED, error: this.errorMessage });
-					}
-				},
-				() => {
-					this.errorMessage = null;
-				}
-			);
-	}
+    GetDesignById(_id: string, flat: boolean = true) {
+        this.ngRedux.dispatch({ type: DESIGN_SELECT_ATTEMPT });
+        this.getDesignByIdSubscription = this.designService.GetDesignById(_id, flat)
+            .map(data => this.ToDesignView(data))
+            .subscribe(
+            (design: IDesignView) => {
+                this.ngRedux.dispatch({ type: DESIGN_SELECT_FULFILLED, payload: design });
+            }, err => {
+                this.errorMessage = err._body;
+                if (this.errorMessage && typeof this.errorMessage === 'string') {
+                    this.ngRedux.dispatch({ type: DESIGN_SELECT_FAILED, error: this.errorMessage });
+                }
+            },
+            () => {
+                this.errorMessage = null;
+            }
+            );
+    }
 
-	CreateDesign(_hostId: string, design: IDesign, flat: boolean = true) {
-		this.ngRedux.dispatch({ type: DESIGN_CREATE_ATTEMPT });
-		this.createDesignSubscription = this.designService.CreateDesign(_hostId, design, flat)
-			.map(data => this.ToDesignView(data))
-			.subscribe(
-				(design: IDesignView) => {
-					this.ngRedux.dispatch({ type: DESIGN_CREATE_FULFILLED, payload: design });
-				}, err => {
-					this.errorMessage = err._body;
-					if (this.errorMessage && typeof this.errorMessage === 'string') {
-						this.ngRedux.dispatch({ type: DESIGN_CREATE_FAILED, error: this.errorMessage });
-					}
-				},
-				() => {
-					this.errorMessage = null;
-				}
-			);
-	}
+    CreateDesign(_hostId: string, design: IDesign, flat: boolean = true) {
+        this.ngRedux.dispatch({ type: DESIGN_CREATE_ATTEMPT });
+        this.createDesignSubscription = this.designService.CreateDesign(_hostId, design, flat)
+            .map(data => this.ToDesignView(data))
+            .subscribe(
+            (design: IDesignView) => {
+                this.ngRedux.dispatch({ type: DESIGN_CREATE_FULFILLED, payload: design });
+            }, err => {
+                this.errorMessage = err._body;
+                if (this.errorMessage && typeof this.errorMessage === 'string') {
+                    this.ngRedux.dispatch({ type: DESIGN_CREATE_FAILED, error: this.errorMessage });
+                }
+            },
+            () => {
+                this.errorMessage = null;
+            }
+            );
+    }
 
-	UpdateDesign(_id: string, design: IDesign, flat: boolean = true) {
-		this.ngRedux.dispatch({ type: DESIGN_UPDATE_ATTEMPT });
-		this.updateDesignSubscription = this.designService.UpdateDesign(_id, design, flat)
-			.map(data => this.ToDesignView(data))
-			.subscribe(
-				(design: IDesignView) => {
-					this.ngRedux.dispatch({ type: DESIGN_UPDATE_FULFILLED, payload: design });
-				}, err => {
-					this.errorMessage = err._body;
-					if (this.errorMessage && typeof this.errorMessage === 'string') {
-						this.ngRedux.dispatch({ type: DESIGN_UPDATE_FAILED, error: this.errorMessage });
-					}
-				},
-				() => {
-					this.errorMessage = null;
-				}
-			);
-	}
+    UpdateDesign(_id: string, design: IDesign, flat: boolean = true) {
+        this.ngRedux.dispatch({ type: DESIGN_UPDATE_ATTEMPT });
+        this.updateDesignSubscription = this.designService.UpdateDesign(_id, design, flat)
+            .map(data => this.ToDesignView(data))
+            .subscribe(
+            (design: IDesignView) => {
+                this.ngRedux.dispatch({ type: DESIGN_UPDATE_FULFILLED, payload: design });
+                this.ngRedux.dispatch({ type: DESIGN_SELECT_FULFILLED, payload: design });
+            }, err => {
+                this.errorMessage = err._body;
+                if (this.errorMessage && typeof this.errorMessage === 'string') {
+                    this.ngRedux.dispatch({ type: DESIGN_UPDATE_FAILED, error: this.errorMessage });
+                }
+            },
+            () => {
+                this.errorMessage = null;
+            }
+            );
+    }
 
-	DeleteDesign(_id: string, design: IDesignView) {
-		this.ngRedux.dispatch({ type: DESIGN_DELETE_ATTEMPT });
-		this.updateDesignSubscription = this.designService.DeleteDesign(_id)
-			.subscribe(
-				data => {
-					this.ngRedux.dispatch({ type: DESIGN_DELETE_FULFILLED, payload: design });
-				}, err => {
-					this.errorMessage = err._body;
-					if (this.errorMessage && typeof this.errorMessage === 'string') {
-						this.ngRedux.dispatch({ type: DESIGN_DELETE_FAILED, error: this.errorMessage });
-					}
-				},
-				() => {
-					this.errorMessage = null;
-				}
-			);
-	}
+    DeleteDesign(_id: string, design: IDesignView) {
+        this.ngRedux.dispatch({ type: DESIGN_DELETE_ATTEMPT });
+        this.updateDesignSubscription = this.designService.DeleteDesign(_id)
+            .subscribe(
+            data => {
+                this.ngRedux.dispatch({ type: DESIGN_DELETE_FULFILLED, payload: design });
+            }, err => {
+                this.errorMessage = err._body;
+                if (this.errorMessage && typeof this.errorMessage === 'string') {
+                    this.ngRedux.dispatch({ type: DESIGN_DELETE_FAILED, error: this.errorMessage });
+                }
+            },
+            () => {
+                this.errorMessage = null;
+            }
+            );
+    }
 
-	SelectDesign(_id: string, flat: boolean = true) {
-		this.ngRedux.dispatch({ type: DESIGN_SELECT_ATTEMPT, });
-		this.selectDesignSubscription = this.designService.GetDesignById(_id, flat)
-		.map(design => this.ToDesignView(design))
-		.subscribe(
-			(design: IDesignView) => {
-				this.ngRedux.dispatch({ type: DESIGN_SELECT_FULFILLED, payload: design });
-			}, err => {
-				this.errorMessage = err._body;
-				if (this.errorMessage && typeof this.errorMessage === 'string') {
-					this.ngRedux.dispatch({ type: DESIGN_SELECT_FAILED, error: this.errorMessage });
-				}
-			},
-			() => {
-				this.errorMessage = null;
-			}
-		);
-	}
+    SelectDesign(_id: string, flat: boolean = true) {
+        this.ngRedux.dispatch({ type: DESIGN_SELECT_ATTEMPT, });
+        this.selectDesignSubscription = this.designService.GetDesignById(_id, flat)
+            .map(design => this.ToDesignView(design))
+            .subscribe(
+            (design: IDesignView) => {
+                this.ngRedux.dispatch({ type: DESIGN_SELECT_FULFILLED, payload: design });
+            }, err => {
+                this.errorMessage = err._body;
+                if (this.errorMessage && typeof this.errorMessage === 'string') {
+                    this.ngRedux.dispatch({ type: DESIGN_SELECT_FAILED, error: this.errorMessage });
+                }
+            },
+            () => {
+                this.errorMessage = null;
+            }
+            );
+    }
 
-	private ToDesignView(data: IDesign): IDesignView {
-		return {
-			_id: data._id,
-			designName: data.designName,
-			colorOne: data.colorOne,
-			colorTwo: data.colorTwo,
-			colorThree: data.colorThree,
-			colorFour: data.colorFour,
-			_host: data['_host._id'],
-			_hostName: data['_host.hostName'],
-			_hostEmail: data['_host.hostEmail'],
-			_profilePic: data['_profilePic._id'],
-			_profilePicSecureUrl: data['_profilePic.secure_url'],
-			_profilePicUrl: data['_profilePic.url']
-		};
-	}
+    private ToDesignView(data: IDesign): IDesignView {
+        return {
+            _id: data._id,
+            designName: data.designName,
+            colorOne: data.colorOne,
+            colorTwo: data.colorTwo,
+            colorThree: data.colorThree,
+            colorFour: data.colorFour,
+            _host: data['_host._id'],
+            _hostName: data['_host.hostName'],
+            _hostEmail: data['_host.hostEmail'],
+            _profilePic: data['_profilePic._id'],
+            _profilePicSecureUrl: data['_profilePic.secure_url'],
+            _profilePicUrl: data['_profilePic.url']
+        };
+    }
 }
