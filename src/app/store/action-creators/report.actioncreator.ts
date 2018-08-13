ac4e412352da0fab821d141893634e91c099d5f8
markatil <markmey.fedalizoatil@gmail.com>
2018-08-13 19:22:10 +0800

    [#TRANSLATION]

--- src/app/store/action-creators/report.actioncreator.ts
+++ src/app/store/action-creators/report.actioncreator.ts
@@ -25,6 +25,7 @@ import { ReportService, DialogService, CategoryService } from '../../services';
 import { IReport } from '../../interface/report/report.interface';
 import { IReportView } from '../../interface/report/report-view.interface';
 import { IMainCategory } from '../../interface/category/main-category.interface';
+import { ISubCategory } from '../../interface/category/sub-category.interface';
 import * as moment from 'moment';
 import * as _ from 'lodash';
 
@@ -77,15 +78,38 @@ export class ReportActionCreator implements OnDestroy {
 			);
 	}
 
-	GetLatestReportByHost(_hostId: string) {
+    GetLatestReportByHost(_hostId: string) {
+        const hostId = _hostId;  
 		this.ngRedux.dispatch({ type: REPORT_GET_ATTEMPT });
 		this.getLatestReportSubscription = this.reportService.GetLatestReportByHost(_hostId)
 			.map(data => {
 				return data.map(d => this.ReportToView(d))
 			})
 			.subscribe(
-				(reports: IReportView[]) => {
-					this.ngRedux.dispatch({ type: REPORT_GET_FULFILLED, payload: reports });
+            (reports: IReportView[]) => {
+                console.log(reports)
+                this.categoryService.GetHostMainCategory(hostId).subscribe(
+                    (category: IMainCategory[]) => {
+                        reports = this.mainCategoryListTranslate(reports, category); 
+
+                        this.categoryService.GetGeneralMainCategory('A').subscribe(
+                        (category: IMainCategory[]) => {
+                            reports = this.mainCategoryListTranslate(reports, category); 
+
+                            this.categoryService.GetGeneralMainCategory('B').subscribe(
+                            (category: IMainCategory[]) => {
+                                reports = this.mainCategoryListTranslate(reports, category);
+
+                                this.categoryService.GetGeneralMainCategory('C').subscribe(
+                                (category: IMainCategory[]) => {
+                                    reports = this.mainCategoryListTranslate(reports, category);
+
+                                    this.ngRedux.dispatch({ type: REPORT_GET_FULFILLED, payload: reports });
+                                });
+                            });
+                        });
+                    });
+
 				}, err => {
 					this.errorMessage = err._body;
 					if (this.errorMessage && typeof this.errorMessage === 'string') {
@@ -172,19 +196,31 @@ export class ReportActionCreator implements OnDestroy {
 			.subscribe(
             (report: IReportView) => {
                     this.categoryService.GetHostMainCategory(report._host).subscribe(
-                    (category: IMainCategory[]) => {
-                        _.map(category, function (x) {
-                            if (report._mainCategory == x._id)
-                            {   
-                                _.map(x.translations, function (y) {
-                                    if (y.code == 'nl') {
-                                        report._mainCategoryName = y.word
-                                    }
-                                });                                
-                            }
-                        });
+                        (category: IMainCategory[]) => {
 
-                        this.ngRedux.dispatch({ type: REPORT_SELECT_FULFILLED, payload: report });
+                        report = this.mainCategoryViewTranslate(report, category);                            
+
+                        this.categoryService.GetGeneralMainCategory('A').subscribe(
+                        (category: IMainCategory[]) => {
+                            report = this.mainCategoryViewTranslate(report, category);
+
+                            this.categoryService.GetGeneralMainCategory('B').subscribe(
+                            (category: IMainCategory[]) => {
+                                report = this.mainCategoryViewTranslate(report, category);
+
+                                this.categoryService.GetGeneralMainCategory('C').subscribe(
+                                (category: IMainCategory[]) => {
+                                    report = this.mainCategoryViewTranslate(report, category);
+
+                                    this.categoryService.GetSubCategory(report._mainCategory).subscribe(
+                                    (subcategory: ISubCategory[]) => {
+                                        report = this.subCategoryViewTranslate(report, subcategory);
+
+                                        this.ngRedux.dispatch({ type: REPORT_SELECT_FULFILLED, payload: report });
+                                    });                                    
+                                });
+                            });
+                        });                                          
                     });
                     					
 				}, err => {
@@ -308,5 +344,49 @@ export class ReportActionCreator implements OnDestroy {
 
 	private padLeft(text: string, padChar: string, size: number): string {
 		return (String(padChar).repeat(size) + text).substr((size * -1), size);
-	}
+    }
+
+    private mainCategoryViewTranslate(report: IReportView, category: IMainCategory[]): IReportView {
+        _.map(category, function (x) {
+            if (report._mainCategory == x._id) {
+                _.map(x.translations, function (y) {
+                    if (y.code == report.language) {
+                        report._mainCategoryName = y.word
+                    }
+                });
+            }
+        });
+
+        return report;
+    }
+
+    private subCategoryViewTranslate(report: IReportView, subcategory: ISubCategory[]): IReportView {
+        _.map(subcategory, function (x) {
+            if (report._subCategory == x._id) {
+                _.map(x.translations, function (y) {
+                    if (y.code == report.language) {
+                        report._subCategoryName = y.word
+                    }
+                });
+            }
+        });
+
+        return report;
+    }
+
+    private mainCategoryListTranslate(reports: IReportView[], category: IMainCategory[]): IReportView[] {
+        _.map(reports, function (report) {
+            _.map(category, function (x) {
+                if (report._mainCategory == x._id) {
+                    _.map(x.translations, function (y) {
+                        if (y.code == report.language) {
+                            report._mainCategoryName = y.word
+                        }
+                    });
+                }
+            });
+        });
+
+        return reports;
+    }
 }
