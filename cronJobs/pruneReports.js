3478f12a98b1a69c74d7feccb365892632fbfda6
John <johnhiggins.avila@gmail.com>
2019-09-03 15:58:56 +0800

    Updating cron jobs

--- cronJobs/pruneReports.js
+++ cronJobs/pruneReports.js
@@ -1,21 +1,25 @@
 const request = require('request-promise');
 const config = require('../config');
+const Promise = require('bluebird');
 
 module.exports = function () {
-  const url = config.server.url;
-  const requestUrl = url + '/v3/api/cronJobs/reports';
-  const options = {
-    method: 'DELETE',
-    uri: requestUrl,
-    headers: {
-        /* 'content-type': 'multipart/form-data' */ // Is set automatically
-    }
-};
-  return request(options)
-    .then(() => {
-      console.log('Successfully pruned reports')
-    })
-    .catch((err) => {
-      console.log('ERROR in pruning reports', err);
-    });
+  const urls = config.server.url.split(':|:');
+
+  return Promise.mapSeries(urls, (url) => {
+    const requestUrl = url + '/v3/api/cronJobs/reports';
+    const options = {
+      method: 'DELETE',
+      uri: requestUrl,
+      headers: {
+          /* 'content-type': 'multipart/form-data' */ // Is set automatically
+      }
+    };
+    return request(options)
+      .then(() => {
+        console.log('Successfully pruned reports')
+      })
+      .catch((err) => {
+        console.log('ERROR in pruning reports', err);
+      });
+  });
 };
